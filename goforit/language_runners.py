5d1c13ddddb937f865083a17e8bdeab211a4e0aa
Daniel Lewis <danroblewis@Mac.lan>
2025-09-07 14:19:43 -0700

    Add C to Assembly (gcc -S) and rename old C to Assembly to C to Objdump

--- goforit/language_runners.py
+++ goforit/language_runners.py
@@ -153,6 +153,51 @@ async def run_c_to_asm(code: str) -> CodeResult:
     with tempfile.NamedTemporaryFile(suffix='.c', mode='w', delete=False) as f:
         f.write(code)
         c_file = f.name
+    
+    try:
+        # Generate assembly output
+        asm_result = await run_process(['gcc', '-S', '-masm=intel', '-o', '-'] + compiler_flags + [c_file])
+        if asm_result.return_code != 0:
+            return asm_result
+
+        # Compile for execution
+        executable = c_file + '.out'
+        compile_result = await run_process(['gcc', c_file, '-o', executable] + compiler_flags)
+        if compile_result.return_code != 0:
+            return compile_result
+        
+        # Run the program
+        run_result = await run_process([executable])
+        
+        # Return assembly and program output
+        return CodeResult(
+            stdout=run_result.stdout,
+            stderr=run_result.stderr,
+            return_code=run_result.return_code,
+            code_outputs=[
+                CodeOutput(content=asm_result.stdout, language="asm-intel")
+            ]
+        )
+    
+    finally:
+        # Cleanup
+        try:
+            os.unlink(c_file)
+            os.unlink(executable)
+        except:
+            pass
+
+async def run_c_to_objdump(code: str) -> CodeResult:
+    # Extract compiler flags from first line comment if present
+    lines = code.split('\n')
+    compiler_flags = []
+    if lines and lines[0].startswith('//'):
+        compiler_flags = lines[0].lstrip('/ ').split()
+    
+    # Create temp files
+    with tempfile.NamedTemporaryFile(suffix='.c', mode='w', delete=False) as f:
+        f.write(code)
+        c_file = f.name
         base_name = os.path.basename(c_file)
     
     try:
@@ -344,6 +389,7 @@ LANGUAGE_RUNNERS = {
     'cpp': run_cpp,
     'c': run_c,
     'c_to_asm': run_c_to_asm,
+    'c_to_objdump': run_c_to_objdump,
     'rust': run_rust,
     'go': run_go
 }
