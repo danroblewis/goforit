33e798d903c15ac5d83b8557b9f4c4b1175f29e6
Daniel Lewis <daniel.robert.lewis@gmail.com>
2025-09-07 14:41:15 -0700

    Add custom assembly language syntax highlighting

--- goforit/static/js/app.js
+++ goforit/static/js/app.js
@@ -15,6 +15,105 @@ export class App {
                 require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.44.0/min/vs' } });
                 require(['vs/editor/editor.main'], (monaco) => {
                     this.monaco = monaco;
+                    
+                    // Register assembly language
+                    monaco.languages.register({ id: 'asm' });
+                    monaco.languages.setMonarchTokensProvider('asm', {
+                        defaultToken: '',
+                        tokenPostfix: '.asm',
+
+                        // Brackets and operators
+                        brackets: [
+                            { token: 'delimiter.curly', open: '{', close: '}' },
+                            { token: 'delimiter.square', open: '[', close: ']' },
+                            { token: 'delimiter.parenthesis', open: '(', close: ')' },
+                            { token: 'delimiter.angle', open: '<', close: '>' }
+                        ],
+
+                        // Assembly keywords
+                        keywords: [
+                            'section', 'global', 'extern', 'align',
+                            'db', 'dw', 'dd', 'dq', 'times', 'equ',
+                            'byte', 'word', 'dword', 'qword'
+                        ],
+
+                        // Common instructions
+                        instructions: [
+                            'mov', 'push', 'pop', 'lea', 'call', 'ret',
+                            'add', 'sub', 'mul', 'div', 'inc', 'dec',
+                            'and', 'or', 'xor', 'not', 'neg', 'shl', 'shr',
+                            'jmp', 'je', 'jne', 'jg', 'jge', 'jl', 'jle',
+                            'cmp', 'test',
+                            'syscall', 'int'
+                        ],
+
+                        // Registers
+                        registers: [
+                            // x86_64
+                            'rax', 'rbx', 'rcx', 'rdx', 'rsi', 'rdi', 'rbp', 'rsp',
+                            'r8', 'r9', 'r10', 'r11', 'r12', 'r13', 'r14', 'r15',
+                            // x86
+                            'eax', 'ebx', 'ecx', 'edx', 'esi', 'edi', 'ebp', 'esp',
+                            // ARM64
+                            'x0', 'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7',
+                            'x8', 'x9', 'x10', 'x11', 'x12', 'x13', 'x14', 'x15',
+                            'x16', 'x17', 'x18', 'x19', 'x20', 'x21', 'x22', 'x23',
+                            'x24', 'x25', 'x26', 'x27', 'x28', 'x29', 'x30',
+                            'sp', 'pc', 'xzr'
+                        ],
+
+                        // Tokenizer
+                        tokenizer: {
+                            root: [
+                                // Labels
+                                [/^[.a-zA-Z_]\w*:/, 'type.identifier'],
+
+                                // Comments
+                                [/;.*$/, 'comment'],
+                                [/\/\/.*$/, 'comment'],
+
+                                // Numbers
+                                [/\b\d*\.\d+([eE][-+]?\d+)?\b/, 'number.float'],
+                                [/\b0x[0-9a-fA-F]+\b/, 'number.hex'],
+                                [/\b\d+\b/, 'number'],
+
+                                // String literals
+                                [/'([^'\\]|\\.)*$/, 'string.invalid'],
+                                [/'/, 'string', '@string'],
+
+                                // Keywords
+                                [/\b(section|global|extern|align)\b/, 'keyword'],
+                                [/\b(db|dw|dd|dq|times|equ)\b/, 'keyword'],
+                                [/\b(byte|word|dword|qword)\b/, 'keyword'],
+
+                                // Instructions
+                                [new RegExp(`\\b(${this.instructions.join('|')})\\b`), 'keyword.instruction'],
+
+                                // Registers
+                                [new RegExp(`\\b(${this.registers.join('|')})\\b`), 'variable.predefined'],
+
+                                // Operators
+                                [/[+\-*/=<>|&^~!]+/, 'operator'],
+
+                                // Identifiers
+                                [/[a-zA-Z_]\w*/, {
+                                    cases: {
+                                        '@keywords': 'keyword',
+                                        '@instructions': 'keyword.instruction',
+                                        '@registers': 'variable.predefined',
+                                        '@default': 'identifier'
+                                    }
+                                }]
+                            ],
+
+                            string: [
+                                [/[^\\']+/, 'string'],
+                                [/\\./, 'string.escape.invalid'],
+                                [/'/, 'string', '@pop']
+                            ]
+                        }
+                    });
+
                     resolve();
                 });
             };
@@ -30,7 +129,7 @@ export class App {
             case 'c_to_objdump':
                 return 'c';
             case 'assembly':
-                return 'nasm';  // Use NASM mode for assembly syntax highlighting
+                return 'asm';  // Use our custom assembly language mode
             default:
                 return language;
         }
